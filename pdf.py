841d8d2a5bf8e4e8265e4b09cf8d6bf277f5ab99
Dylan Alfonso <alfonso.d@wustl.edu>
2023-07-13 15:28:55 -0500

    limited PDF to 10,000 characters being passed

--- pdf.py
+++ pdf.py
@@ -5,13 +5,17 @@ import openai
 
 def pdf_summary(pdf_file):
     pdf_reader = PyPDF2.PdfReader(pdf_file)
-
+    max_characters = 10000
     num_pages = len(pdf_reader.pages)
     extracted_data = []
+    total_characters = 0
 
     for page_num in range(num_pages):
+        if total_characters >= max_characters:
+            break
         page = pdf_reader.pages[page_num]
         extracted_data.append(page.extract_text())
+        total_characters += len(page.extract_text())
 
     json_data = json.dumps(extracted_data)
     json_output_path = 'path_to_output_json_file.json'
