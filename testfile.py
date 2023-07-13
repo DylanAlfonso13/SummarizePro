254d584c127199b4d9dbb42f7df30cc3c7e6f583
Dylan Alfonso <alfonso.d@wustl.edu>
2023-07-13 12:49:27 -0500

    Fixed testing style and directory for unittests

--- testfile.py
+++ testfile.py
@@ -1,6 +1,8 @@
-import unittest, sys
+import unittest
+import sys
 from app import app
 sys.path.append('../SummarizePro')
+
 
 class TestFile(unittest.TestCase):
 
@@ -26,6 +28,7 @@ class TestFile(unittest.TestCase):
     def test_fake_route(self):
         response = app.test_client().get('/;aldkfj')
         self.assertNotEqual(response.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
